b852ff60fa3a4c512a6b06377e49c4cf9555cd24
Sumit K <synergy2411@outlook.com>
2022-11-08 11:17:09 +0530

    practice exercise added

--- 08-misc-topics/app.js
+++ 08-misc-topics/app.js
@@ -1,3 +1,22 @@
+const navList = document.querySelector(".nav-list")
+
+navList.addEventListener("click", function (event) {
+    console.log(event.target.href)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const parentDiv = document.querySelector(".parent")
 const childDiv = document.querySelector(".child")
 
